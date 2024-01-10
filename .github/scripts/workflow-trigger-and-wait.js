4768def1661b7c3d1869a6f42157fc3930d63bcd
ldraney <draneylucas@gmail.com>
2024-01-10 15:27:16 -0700

    getting job id

--- .github/scripts/workflow-trigger-and-wait.js
+++ .github/scripts/workflow-trigger-and-wait.js
@@ -3,6 +3,7 @@ const triggerAndWait = async ({ github, context }) => {
   const repo = 'private-repo'; // the private repo 
   const workflow_id = 'private-workflow.yml'; // Replace with your workflow file name or ID
   const ref = 'master'; // branch of the workflow you want to use
+  const jobName = 'say-hello'; // Replace with the name of the job you want
 
   // Define the inputs required by the workflow
   const inputs = {
@@ -43,7 +44,8 @@ const triggerAndWait = async ({ github, context }) => {
 	}
 
 	// Wait before polling again
-	await new Promise(r => setTimeout(r, 10000));
+	// Github api has a limit of either 1,000 or 15,000, based on if the VA is on GitHub Enterprise Cloud or not.
+	await new Promise(r => setTimeout(r, 1000));
   }
 
   console.log(`Triggered workflow run ID: ${run_id}`);
@@ -74,18 +76,33 @@ const triggerAndWait = async ({ github, context }) => {
 	run_id: run_id, // Replace with the actual run ID
   });
 
+  const job = jobs.data.jobs.find(j => j.name === jobName);
+  if (!job) {
+	console.log(`Job '${jobName}' not found in workflow run.`);
+	return;
+  }
+
   // Fetch the logs or outputs if necessary
   if (conclusion === 'success') {
-    // If your workflow produces artifacts, you can fetch them here
-    const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-      owner,
-      repo,
-      run_id,
-    });
-    console.log('Artifacts:', artifacts.data.artifacts);
-    // Additional processing to download or extract data from artifacts can be done here
+	  const job_logs = github.rest.actions.downloadJobLogsForWorkflowRun({
+	  owner,
+	  repo,
+	  job_id,
+	  });
+
+	  console.log(${job_logs})
+
+    // // If your workflow produces artifacts, you can fetch them here
+    // const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
+      // owner,
+      // repo,
+      // run_id,
+    // });
+    // console.log('Artifacts:', artifacts.data.artifacts);
+    // // Additional processing to download or extract data from artifacts can be done here
   } else {
-    console.log('Workflow failed. No artifacts fetched.');
+    // console.log('Workflow failed. No artifacts fetched.');
+	console.log('cannot find logs')
   }
 };
 
